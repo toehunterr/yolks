6a982981b54fb01ee23870fcfbc5d67d6f6d0dda
toehunterr <104683985+toehunterr@users.noreply.github.com>
2025-02-02 17:32:18 +1100

    Update entrypoint.sh

--- games/core-keeper/entrypoint.sh
+++ games/core-keeper/entrypoint.sh
@@ -13,7 +13,7 @@ cd /home/container
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-./parser.sh
+/bin/bash /parser.sh
 
 ## just in case someone removed the defaults.
 if [ "${STEAM_USER}" == "" ]; then
