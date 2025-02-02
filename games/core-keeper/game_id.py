786c8deb157ad12a7b37683c8f26905489dafdba
toehunterr <104683985+toehunterr@users.noreply.github.com>
2025-02-02 16:41:49 +1100

    Update game_id.py

--- games/core-keeper/game_id.py
+++ games/core-keeper/game_id.py
@@ -2,24 +2,24 @@ import re
 import time
 import os
 import sys
+import signal
 
 def monitor_log():
-    while True:
-        if os.path.exists("CoreKeeperServerLog.txt"):
-            try:
-                with open("CoreKeeperServerLog.txt", "r") as f:
-                    content = f.read()
-                    match = re.search(r'Started session with Game ID ([a-zA-Z0-9]+)', content)
-                    if match:
-                        game_id = match.group(1)
-                        print("-------------")
-                        print(f"Server Game ID: {game_id}")
-                        print("-------------")
-                        break
-            except Exception as e:
-                print(f"Error reading log file: {e}", file=sys.stderr)
-        
-        time.sleep(5)
+    if os.path.exists("CoreKeeperServerLog.txt"):
+        try:
+            with open("CoreKeeperServerLog.txt", "r") as f:
+                content = f.read()
+                match = re.search(r'Started session with Game ID ([a-zA-Z0-9]+)', content)
+                if match:
+                    game_id = match.group(1)
+                    print("-------------")
+                    print(f"Server Game ID: {game_id}")
+                    print("-------------")
+                    signal.pause()
+        except Exception as e:
+            print(f"Error reading log file: {e}", file=sys.stderr)
 
 if __name__ == "__main__":
-    monitor_log()
+    while True:
+        monitor_log()
+        time.sleep(5)
